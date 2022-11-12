6fd8b8aaac3bfabdf1e9f756c3c5df40bc33a4ed
Elivelton Santos <samsepi0l.dev@gmail.com>
2022-11-11 23:06:47 -0300

    Revert "create tests"
    This reverts commit a4a6bcce8125b12322987e87d6e7e4bd757013ae.

--- server/vitest.config.ts
+++ server/vitest.config.ts
@@ -1,12 +1,5 @@
 import { defineConfig } from 'vitest/config'
-import path from 'path'
 
 export default defineConfig({
-  test: {},
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@/tests': path.resolve(__dirname, './tests')
-    }
-  }
+  test: {}
 })
